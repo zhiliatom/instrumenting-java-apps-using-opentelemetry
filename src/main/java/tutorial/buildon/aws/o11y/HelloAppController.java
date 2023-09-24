23b31772b461658e838f6aa6b83c7e475bc40fd6
zhiliatom <zhiliatox@163.com>
2023-09-25 00:16:55 +0800

    finish lession

--- src/main/java/tutorial/buildon/aws/o11y/HelloAppController.java
+++ src/main/java/tutorial/buildon/aws/o11y/HelloAppController.java
@@ -1,10 +1,24 @@
 package tutorial.buildon.aws.o11y;
 
-import java.util.Objects;
-
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+import static java.lang.Runtime.getRuntime;
+import static tutorial.buildon.aws.o11y.Constants.*;
 
 @RestController
 public class HelloAppController {
@@ -12,15 +26,40 @@ public class HelloAppController {
     private static final Logger log =
         LoggerFactory.getLogger(HelloAppController.class);
 
+    @Value("otel.traces.api.version")
+    private String tracesApiVersion;
+
+    @Value("otel.metrics.api.version")
+    private String metricsApiVersion;
+
+
+    private final Tracer tracer = GlobalOpenTelemetry.getTracer("io.opentelemetry.traces.hello", tracesApiVersion);
+
+    private final Meter meter = GlobalOpenTelemetry.meterBuilder("io.opentelemetry.metrics.hello")
+            .setInstrumentationVersion(metricsApiVersion)
+            .build();
+
+    private LongCounter numberOfExecutions;
+
     @RequestMapping(method= RequestMethod.GET, value="/hello")
     public Response hello() {
         Response response = buildResponse();
+        Span span = tracer.spanBuilder("mySpan").startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            if (response.isValid()) {
+                numberOfExecutions.add(1);
+                log.info("The response is valid.");
+            }
+        } finally {
+            span.end();
+        }
         if (response.isValid()) {
             log.info("The response is valid.");
         }
         return response;
     }
 
+    @WithSpan
     private Response buildResponse() {
         return new Response("Hello World");
     }
@@ -33,5 +72,26 @@ public class HelloAppController {
             return true;
         }
     }
-    
+
+    @PostConstruct
+    public void createMetrics() {
+
+        numberOfExecutions =
+                meter
+                        .counterBuilder(NUMBER_OF_EXEC_NAME)
+                        .setDescription(NUMBER_OF_EXEC_DESCRIPTION)
+                        .setUnit("int")
+                        .build();
+
+        meter
+                .gaugeBuilder(HEAP_MEMORY_NAME)
+                .setDescription(HEAP_MEMORY_DESCRIPTION)
+                .setUnit("byte")
+                .buildWithCallback(
+                        r -> {
+                            r.record(getRuntime().totalMemory() - getRuntime().freeMemory());
+                        });
+
+    }
+
 }
